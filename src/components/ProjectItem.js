149c7d03b28985a0edcf71a33f5bd2e55566c6e9
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-07-16 11:51:45 +0300

    completed

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const renderSpans = technologies.map(tech=> <span key={tech}>{tech}</span>)
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {renderSpans}
+        {/* <projectItem technologies = {project[0].technologies}/>
+            <projectItem technologies = {project[1].technologies}/>
+            <projectItem technologies = {project[2].technologies}/>}*/}
+      
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,19 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const renderProjects = projects.map(project=> {
+    return <ProjectItem key={project.id} name={project.name} about={project.about}
+    technologies={project.technologies}/>
+  })
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {renderProjects}
+        {/*<ProjectItem name={projects[0].name} about={projects[0].about} technologies={projects[0].technologies}/>
+        <ProjectItem name={project[1].name} about={projects[1].about} technologies={projects[1].technologies}/>
+        <ProjectItem name={projects[2].name} about={projects[2].about} technologies={projects[2].technologies}/>*/}
+        </div>
     </div>
   );
 }
